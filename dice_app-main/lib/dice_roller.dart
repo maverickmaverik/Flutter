c067fafda612358588737d53ad4c0f0c16cb0c18
maverickmaverik <handoko.bana@gmail.com>
2024-10-04 22:02:47 +0700

    New update

--- dice_app-main/lib/dice_roller.dart
+++ dice_app-main/lib/dice_roller.dart
@@ -1,44 +1,4 @@
-// import 'dart:math';
-// import 'package:flutter/material.dart';
-
-// final randomizer = Random();
-
-// class DiceRoller extends StatefulWidget {
-//   const DiceRoller({super.key});
-
-//   @override
-//   State<DiceRoller> createState() {
-//     return _DiceRollerState();
-//   }
-// }
-
-// class _DiceRollerState extends State<DiceRoller> {
-//   var currentDiceRoll = 2;
-
-//   void rollDice() {
-//     setState(() {
-//       currentDiceRoll = randomizer.nextInt(6) + 1;
-//     });
-//   }
-
-//   @override
-//   Widget build(context) {
-//     return Center(
-//       child: TextButton(
-//         onPressed: rollDice,
-//         child: Image.asset(
-//           'assets/images/dice-$currentDiceRoll.png',
-//           width: 200,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// Auto Random Dice Roller
 import 'dart:math';
-import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 final randomizer = Random();
@@ -47,76 +7,28 @@ class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
   @override
-  State<DiceRoller> createState() => _DiceRollerState();
+  State<DiceRoller> createState() {
+    return DiceRollerState(); 
+  }
 }
 
-class _DiceRollerState extends State<DiceRoller> {
+class DiceRollerState extends State<DiceRoller> {
   var currentDiceRoll = 2;
-  Timer? _timer;
-  bool isRolling = false;
 
-  @override
-  void initState() {
-    super.initState();
-    startRolling();
-  }
-
-  void startRolling() {
+  void rollDice() {
     setState(() {
-      isRolling = true;
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
-    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      setState(() {
-        currentDiceRoll = randomizer.nextInt(6) + 1;
-      });
-    });
-    if (kDebugMode) {
-      print('Rolling started. isRolling: $isRolling');
-    }
-  }
-
-  void stopRolling() {
-    _timer?.cancel();
-    setState(() {
-      isRolling = false;
-    });
-    if (kDebugMode) {
-      print('Rolling stopped. isRolling: $isRolling');
-    }
-  }
-
-  void toggleRolling() {
-    if (isRolling) {
-      stopRolling();
-    } else {
-      startRolling();
-    }
   }
 
   @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: toggleRolling,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/dice-$currentDiceRoll.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              isRolling ? 'Click to Stop' : 'Click to Start',
-              style: const TextStyle(fontSize: 18),
-            ),
-          ],
+  Widget build(context) {
+    return Center(
+      child: TextButton(
+        onPressed: rollDice,
+        child: Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
         ),
       ),
     );
--- dice_app-main/lib/gradient_container.dart
+++ dice_app-main/lib/gradient_container.dart
@@ -4,29 +4,50 @@ import 'package:dice_app/dice_roller.dart';
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-class GradientContainer extends StatelessWidget {
+class GradientContainer extends StatefulWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
-
-  // Example of Named Constructor
-  const GradientContainer.purple({super.key})
-      : color1 = Colors.deepPurple,
-        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
 
   @override
-  Widget build(context) {
+  State<GradientContainer> createState() => _GradientContainerState();
+}
+
+class _GradientContainerState extends State<GradientContainer> {
+  final GlobalKey<DiceRollerState> _diceKey1 = GlobalKey<DiceRollerState>();
+  final GlobalKey<DiceRollerState> _diceKey2 = GlobalKey<DiceRollerState>();
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2],
+          colors: [widget.color1, widget.color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: DiceRoller(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DiceRoller(key: _diceKey1), 
+              const SizedBox(width: 40),  
+              DiceRoller(key: _diceKey2), 
+            ],
+          ),
+          const SizedBox(height: 40), 
+          ElevatedButton(
+            onPressed: () {
+              _diceKey1.currentState?.rollDice();
+              _diceKey2.currentState?.rollDice();
+            },
+            child: const Text('Roll the Dice!'),
+          ),
+        ],
       ),
     );
   }
