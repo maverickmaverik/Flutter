3e03ceb081e8f079dc06cfb6f783a2f5654cfadc
maverickmaverik <141262399+maverickmaverik@users.noreply.github.com>
2025-12-16 09:50:22 +0700

    Tugas Rancang Mobile Application Development for Engineering

--- fav_location_layout/android/app/src/main/kotlin/com/example/fav_location_layout/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fav_location_layout
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- note_taking_application/android/app/src/main/kotlin/com/example/note_taking_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.note_taking_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
